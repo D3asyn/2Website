f0e0fddbc439c0ac95e6c1256818781c0c26cffa
D3asyn <konkolykovacsd@gmail.com>
2024-05-16 10:16:58 +0200

    init 2

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import express from "express";
+const express = require('express');
+const routes = require('./routes'); // Új modul importálása
+const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 5000; // Port beolvasása a .env fájlból, alapértelmezett érték: 8000
 
-app.get("/", function (req, res) {
-	res.render("index.ejs");
-});
+// Statikus fájlok kiszolgálása
+app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
-app.listen(port, function () {
-	console.log("Server listening on port " + port);
+app.set('view engine', 'ejs'); // EJS beállítása
+app.use(routes); // Új modul használata az útvonalakhoz
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log('Hiba történt a szerver indításakor');
+  } else {
+    console.log(`Az alkalmazás fut a(z) ${port}-es porton`);
+  }
 });
